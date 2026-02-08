db00a119458e65f9bed51e4d1032a2dd663579b3
Moiz Ali <moizk5590@gmail.com>
2026-02-08 18:03:12 +0500

    vercel.json update

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
+# Root directory ko system path mein add karein
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from backend.main import app
--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./medical.db"
+# --- CHANGE IS HERE ---
+# Agar Vercel par hai, to /tmp folder use karein (Temporary Storage)
+if os.environ.get("VERCEL"):
+    SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/patients.db"
+else:
+    # Localhost ke liye wahi purana path
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./patients.db"
+# ----------------------
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -10,3 +18,10 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
